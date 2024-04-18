56f25bf5e6703006e1cdb7f5f49640d017ff67f7
THIAGO VALE <consultoria.thiagovale@gmail.com>
2024-04-18 19:58:29 -0300

    Add files via upload

---
+++ handler.py
@@ -0,0 +1,33 @@
+import pickle
+import pandas as pd
+from flask import Flask, request, Response
+from rossmann.Rossmann import Rossmann
+import os
+
+model = pickle.load( open( './model/model_xgb_tuned.pkl', 'rb') )
+
+app = Flask( __name__ )
+
+@app.route( '/rossmann/predict', methods=['POST'] )
+
+def rossmann_predict():
+    test_json = request.get_json()
+    if test_json:
+        if isinstance( test_json, dict ): 
+            test_raw = pd.DataFrame( test_json, index=[0] )
+        else: 
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
+        pipeline = Rossmann()
+        df1 = pipeline.data_clean( test_raw )
+        df2 = pipeline.feature_engineering( df1 )
+        df3 = pipeline.data_preparation( df2 )
+        df_response = pipeline.get_predicitons( model, test_raw, df3 )
+
+        return df_response
+
+    else:
+        return Response( '{}', status=200, mimetype='application/json' )
+    
+if __name__ == '__main__':
+    port = os.environ( 'PORT', 5000 )
+    app.run( '0.0.0.0', port=port )
