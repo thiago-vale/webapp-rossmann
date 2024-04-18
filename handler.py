a12520ce051c42d4f00920344ac68927452fcd4c
thiago-vale <consultoria.thiagovale@gmail.com>
2024-04-18 20:40:24 -0300

    repair error in port

--- handler.py
+++ handler.py
@@ -29,5 +29,5 @@ def rossmann_predict():
         return Response( '{}', status=200, mimetype='application/json' )
     
 if __name__ == '__main__':
-    port = os.environ( 'PORT', 10000 )
+    port = os.environ.get( 'PORT', 5000 )
     app.run( '0.0.0.0', port=port )
