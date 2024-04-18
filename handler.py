0e57ec15bbb37b632a887e0328c89fe662a63f24
thiago-vale <consultoria.thiagovale@gmail.com>
2024-04-18 20:17:57 -0300

    mudando porta

--- handler.py
+++ handler.py
@@ -29,5 +29,5 @@ def rossmann_predict():
         return Response( '{}', status=200, mimetype='application/json' )
     
 if __name__ == '__main__':
-    port = int(os.environ( 'PORT', 5000 ))
+    port = os.environ( 'PORT', 10000 )
     app.run( '0.0.0.0', port=port )
